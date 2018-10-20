c6e2df6a2e0bd544a04dff4d09194535c5fdd4c5
tradesdr <tysabs@gmail.com>
2018-10-20 13:15:52 -0500

    Move page back to top on navigation to new page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'code-club';
+export class AppComponent implements OnInit{
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // jump to top of page whenever a nav-link is used
+    this.router.events.subscribe((event) => {
+      if (!(event instanceof NavigationEnd)) {
+        return;
+      } else {
+        document.body.scrollTop = 0;
+      }
+    });
+  };
 }
